1d70e15bceab525cdfd16f3d3e4e9049cf2724c7
nopall <nouvalfarras15@gmail.com>
2020-11-21 09:24:52 +0700

    map

--- pythonfold/args_kwargs.py
+++ pythonfold/args_kwargs.py
@@ -1,1 +1,11 @@
-#
+#*args atau argument digunakan untuk menggantikan parameter yang banyak dan panjang
+def jumlah(*args):
+    return sum((args))
+
+print(jumlah(1,2,3,4,5,6,7,8,9))
+
+#**kwargs adalah keywords argument berisi dictionary
+def definisikan(**kwargs):
+    print(kwargs)
+
+definisikan(jerapah="tinggi")
